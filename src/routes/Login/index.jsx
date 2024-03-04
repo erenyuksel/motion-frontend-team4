12f13527cacd9e3efb34dcf3205744fbdc97a254
Vytas <vytas.urbelis@gmail.com>
2024-03-04 17:07:40 +0100

    Main_login page

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,8 +1,26 @@
 import LoginChild from "../../components/Login/Login_child"
+import './styles.scss'
 
 const Login = () => {
     return (
-        <LoginChild/>
+    <div className="login_main_container">
+        <div className="login_left_container">
+            <div className="login_logo"><img src="./src/assets/images/logo_white.png" alt="Motion" /></div>
+            <div className="login_motion">Motion</div>
+            <div className="login_about"><p>Conect with friends and the world around you with Motion</p></div>
+            <div className="app_store_buttons">
+                <div className="store_button"><img src="./src/assets/svgs/apple.svg" alt="Motion" /></div>
+                <div className="store_button"><img src="./src/assets/images/logo_white.png" alt="Motion" /></div>
+            </div>
+            <div className="links">
+                <div className="social_links">twittwe</div>
+                <div className="social_links">facebook</div>
+                <div className="social_links">instagram</div>
+            </div>
+            <div className="rights_reserved">@ Motion 2018. All rights reserved</div>
+        </div>
+        <div className="login_right_container"><LoginChild/></div>
+    </div>
     )
 }
 
