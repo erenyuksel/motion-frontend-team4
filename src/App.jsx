8b03d26f55b485fd65bea22447673acee2532395
Vytas <vytas.urbelis@gmail.com>
2024-03-04 11:50:18 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import './App.scss'
+import LoginChild from './components/Login/Login_child'
+
+function App() {
+
+
+  return (
+    <>
+    <LoginChild/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login/Login_child/index.jsx
@@ -0,0 +1,6 @@
+import './styles.scss'
+const LoginChild = () => {
+    return (<div className="test"> Test </div>)
+}
+
+export default LoginChild 
