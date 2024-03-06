e3a9f676fe7d1de231509132371445521ca4aa89
Vytas <vytas.urbelis@gmail.com>
2024-03-06 19:34:02 +0100

    fixing conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-import { useDispatch } from "react-redux";
-import "./App.scss";
-import Router from "./routes";
-import { useEffect, useState } from "react";
-import AxiosUser from "./axios/UserAxios";
-import { userLogin, userLogout } from "./store/UserSlice";
-import Spinner from "./components/Spinner";
-=======
+
 import { useDispatch} from 'react-redux'
 import './App.scss'
 import Router from './routes'
@@ -14,16 +6,12 @@ import { useEffect, useState } from 'react'
 import AxiosUser, { getMyProfileData } from './axios/UserAxios'
 import { userLogin, userLogout, userObject } from './store/UserSlice'
 import Spinner from './components/Spinner'
->>>>>>> master
 
 function App() {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-<<<<<<< HEAD
-    const token = window.localStorage.getItem("token");
-=======
     const token = window.localStorage.getItem('token')
     
     const verify = async () => {
@@ -42,22 +30,6 @@ function App() {
     }
     token ? verify() : dispatch(userLogout())
   }, [])
->>>>>>> master
-
-    const verify = async () => {
-      setIsLoading(true);
-      try {
-        await AxiosUser.post("/auth/token/verify/", { token: token });
-        dispatch(userLogin(token));
-      } catch (error) {
-        window.localStorage.removeItem("token");
-        dispatch(userLogout());
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    token ? verify() : dispatch(userLogout());
-  }, []);
 
   if (!isLoading) {
     return (
