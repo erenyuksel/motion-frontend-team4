b665ea4c882c33d3fd3542219aa2c8d7e74de48f
Vytas <vytas.urbelis@gmail.com>
2024-03-07 08:49:25 +0100

    Finished user page

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,8 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.scss";
-import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import AxiosUser from "../../axios/UserAxios";
+
 const ProfileComponent = ({ avatar, user }) => {
   const [bottom, setBottom] = useState("posts");
+  const [posts, setPosts] = useState([]);
+  const [visitingUser, setVisitingUser] = useState(user);
+  const token = useSelector((state) => state.user.token);
+  const LogedInUser = useSelector((state) => state.user.user);
+
+  useEffect(() => {
+    const getUserPosts = async () => {
+      try {
+        const data = await AxiosUser.get(
+          `/social/posts/user/${user.id}/?limit=6&offset=1`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setPosts(data.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUserPosts();
+  }, []);
+
+  const follow = async () => {
+    const response = await AxiosUser.post(
+      `/social/followers/toggle-follow/${visitingUser.id}/`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    setVisitingUser(response.data);
+  };
+
+  const sendFriendRequest = async () => {
+    const response = await AxiosUser.post(
+      `/social/friends/request/${visitingUser.id}/`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    setVisitingUser(response.data.receiver);
+  };
 
   return (
     <div className="page_container">
@@ -10,41 +63,70 @@ const ProfileComponent = ({ avatar, user }) => {
       <div className="profile_container">
         <div className="profile_all_data">
           <div className="profile">
-            <Link to='/friend-profile/794'><div className="profile_avatar">
-              <img src={avatar} alt="" />
-            </div> </Link>
+              <div className="profile_avatar">
+                <img src={avatar} alt="" />
+              </div>{" "}
             <div className="profile_name">
               {user.first_name} {user.last_name}
             </div>
             <div className="profile_location">{user.location}</div>
-            <div className="edit_profile">EDIT PROFILE</div>
+            {LogedInUser.id === user.id ? (
+              <div className="profile_button">EDIT PROFILE</div>
+            ) : (
+              <div>
+                {visitingUser.logged_in_user_is_following ? (
+                  <div onClick={follow} className="profile_button">
+                    FOLLOWING
+                  </div>
+                ) : (
+                  <div onClick={follow} className="profile_button">
+                    FOLLOW
+                  </div>
+                )}
+
+                {visitingUser.logged_in_user_is_friends ? (
+                  <div className="profile_button_disabled">FRIEND</div>
+                ) : !visitingUser.logged_in_user_is_friends && !visitingUser.logged_in_user_sent_fr ? (
+                  <div onClick={sendFriendRequest} className="profile_button">
+                    ADD FRIEND
+                  </div>
+                ) : visitingUser.logged_in_user_is_rejected ? (
+                  <div className="profile_button_disabled">REJECTED</div>
+                ) : visitingUser.logged_in_user_sent_fr ? (
+                    <div className="profile_button_disabled">REQUEST SENT</div>
+                ) : <div></div>}
+              </div>
+            )}
           </div>
           <div className="profile_data">
             <div className="rest_profile_data">
               <div className="about_email_phone">
                 <div className="profile_about">
-                  About:
-                  <br />
+                  <div className="column_name">About:</div>
                   {user.about_me}
                 </div>
                 <div className="email_phone">
                   <div className="profile_email">
-                    Email:
-                    <br />
+                    <div className="column_name">Email:</div>
+
                     {user.email}
                   </div>
                   <div className="profile_phone">
-                    Phone:
-                    <br />
+                    <div className="column_name">Phone:</div>
+
                     {user.phone_number}
                   </div>
                 </div>
               </div>
               <div className="things_i_like">
-                Things I like:
-                {user.things_user_likes.map((item) => (
-                  <div key={item}>{item}</div>
-                ))}
+                <div className="column_name">Things I like:</div>
+                <div className="liked_things">
+                  {user.things_user_likes.map((item) => (
+                    <div className="thing" key={item}>
+                      {item}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
             <div className="numbers">
